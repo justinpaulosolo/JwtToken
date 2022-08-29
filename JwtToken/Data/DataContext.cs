48d0c727c0917360c14c5c0d89c833304b886e00
Justin Solo <justinpaulosolo@gmail.com>
2022-08-28 21:29:19 -0700

    Add project files.

---
+++ JwtToken/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using JwtToken.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtToken.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Server=.\\SQLExpress;Database=userjwt;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ JwtToken/Models/UserRegistrationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtToken.Models
+{
+    public class UserRegistrationRequest
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string PasswordConfirm { get; set; } = string.Empty;
+    }
+}
